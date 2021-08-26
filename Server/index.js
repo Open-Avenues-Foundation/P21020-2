7b90d4bb3c98a67ebec4a81e1f7967f21de91a3b
kjmatt91 <kjmatt91@gmail.com>
2021-08-25 20:04:30 -0400

    got all main files set up and beginning code

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(8080, () => {
+  console.log('Listening on 8080 :)')
+})
