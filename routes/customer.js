b14874abf5fa0884ec3019c1a71f45061bd4468f
Ignacio Ojanguren <ignacio@privy.com>
2021-09-15 22:00:01 -0400

    Setup skeleton for customers

---
+++ routes/customer.js
@@ -0,0 +1,26 @@
+const { getCustomers, saveCustomers } = require('../controllers/customerController');
+
+// Customer routes is responsible to define the routes for the customer
+const routes = app => {
+  // Two of the routes we want to create are:
+  // 1) Route to return all the customers
+  // '/customers'
+  // What type of request are we expecting? GET, POST, PATH, DELETE, and why?
+  app.('', (req, res) => {
+    const customers = // How do we get the customers?
+    res.send(customers)
+  })
+
+  // 2) Route to save customers in bulk
+  // '/uploadCustomers'
+  // What type of request are we expecting? GET, POST, PATH, DELETE, and why?
+  app.('', (req, res) => {
+    const customersList = // How do we access the request to retrieve the customers?
+
+    // Calling the controller to handle the logic on how to save the customers.
+    saveCustomers(customersList)
+    res.send('Customers save successfully')
+  });
+};
+
+module.exports = routes;
