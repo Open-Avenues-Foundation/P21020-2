43b9e2b8f3d9e8cb91d8401c174d668ae2e2731a
kjmatt91 <kjmatt91@gmail.com>
2021-09-27 23:01:03 -0400

    Set up body content with customer list and data on react end

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import BodyContent from '../src/components/BodyContent'
+
 
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>Privey</h1>
       <h3>SMS Marketing Dashboard</h3>
     </div>
+    <BodyContent/>
+    </>
   );
 }
 
