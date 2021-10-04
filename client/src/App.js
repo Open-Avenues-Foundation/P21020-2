c319b064aa9a602aaf19f9b90378c7ff61698d02
Mohamed Hafez <mhafez@Mohameds-MacBook-Pro.local>
2021-10-03 22:04:11 -0400

    adding bootstrap and mui ui as well as general layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
-import BodyContent from '../src/components/BodyContent'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+import CustomersList from './components/CustomerList'
 
 
 function App() {
   return (
-    <>
     <div className="App">
-      <h1>Privey</h1>
-      <h3>SMS Marketing Dashboard</h3>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col col1">
+            <h2>Show List of customers</h2>
+            <h5>Or show msg asking for CSV to upload list of customers</h5>
+            <CustomersList />
+          </div>
+          <div className="col col2">
+            <h2>Create Message Here</h2>
+            <p>
+              We should have should be able to create the msg actually
+              typing it out , select to whome we will be sending
+              when we type msg we have to be mindful of how many characters sms msg can have ?
+            </p>
+          </div>
+          <div className="col col3">
+            <h2>Display Message Sent Status here</h2>
+            <p> in this section we will display either green KO messages sent or Red there is a failure</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-    <BodyContent/>
-    </>
   );
 }
 
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Footer extends React.Component {
+    render () {
+      return <div className='Footer'>
+        <p>All rights reserved to Privey &copy; 2021</p>
+      </div>
+    }
+  }
+
+ 
+
+  export default Footer
